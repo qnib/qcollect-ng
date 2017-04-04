476982cf6bda3c90674f140f97f4843ab080427c
Christian Kniep <christian@qnib.org>
2017-04-04 18:50:22 +0200

    Bump version: 0.3.2.2 → 0.3.2.3

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "ETC metrics collector based on qframe, inspired by qcollect,logstash and fullerite"
 	app.Usage = "qcollect-ng [options]"
-	app.Version = "0.3.2.2"
+	app.Version = "0.3.2.3"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "config",
