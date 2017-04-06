3378efbc7202a1a0ae0d3abbcc3f85651fafa986
Christian Kniep <ChristianKniep@users.noreply.github.com>
2017-04-06 20:40:00 +0200

    Bugfix docker stats 14 (#15)
    * disabled stats channel clears #14
    * disabled CPU metrics to not panic (#14)

--- collectors/docker-stats/plugin.go
+++ collectors/docker-stats/plugin.go
@@ -4,6 +4,8 @@ import (
 	"C"
 	"log"
 	"fmt"
+	"sync"
+	"regexp"
 
 	"github.com/docker/docker/client"
 	"golang.org/x/net/context"
@@ -12,14 +14,13 @@ import (
 	"github.com/pkg/errors"
 	"github.com/zpatrick/go-config"
 
-	//"github.com/qnib/qcollect-ng/types"
-	//"github.com/qnib/"
+	"github.com/qnib/qcollect-ng/utils"
+	"github.com/qnib/qcollect-ng/types"
 	fTypes "github.com/qnib/qframe/types"
-	"sync"
 )
 
 
-//var SuperMap = make(map[string]ContainerSupervisor)
+var SuperMap = make(map[string]ContainerSupervisor)
 
 // struct to keep info and channels to goroutine
 // -> get heartbeats so that we know it's still alive
@@ -30,15 +31,17 @@ type ContainerSupervisor struct {
 	Com 	chan interface{} // Channel to communicate with goroutine
 	cli 	*docker.Client
 	qChan 	fTypes.QChan
+	IgnoreRegex *regexp.Regexp
 }
 
-func NewCntSuper(cli *docker.Client, qChan fTypes.QChan, cntID, cntName string) ContainerSupervisor {
+func NewCntSuper(cli *docker.Client, qChan fTypes.QChan, cntID, cntName string, ignReg *regexp.Regexp) ContainerSupervisor {
 	return ContainerSupervisor{
 		CntID: cntID,
 		CntName: cntName,
 		Com: make(chan interface{}),
 		cli: cli,
 		qChan: qChan,
+		IgnoreRegex: ignReg,
 	}
 }
 
@@ -61,15 +64,20 @@ func (cs ContainerSupervisor) Run() {
 	for {
 		select {
 		case msg := <-cs.Com:
-			log.Printf("Got message from cs.Com: %v\n", msg)
+			switch msg {
+			case "died":
+				log.Printf("[DD] Container [%s]->'%s' died -> BYE!", cs.CntID, cs.CntName)
+				return
+			default:
+				log.Printf("[DD] Container [%s]->'%s' got message from cs.Com: %v\n", cs.CntID, cs.CntName, msg)
+			}
 		case stats, ok := <-statsChannel:
 			if !ok {
 				err := errors.New(fmt.Sprintf("Bad response getting stats for container: %s", cs.CntID))
 				log.Println(err.Error())
 				return
 			}
-			_ = stats
-			/*
+
 			dim := map[string]string{
 				"container_id":   cs.CntID,
 				"container_name": cs.CntName,
@@ -83,27 +91,30 @@ func (cs ContainerSupervisor) Run() {
 				dim["task_slot"] = task.Slot
 				dim["service_name"] = task.Name
 			}
+
+			/*
+			//////// Panic when this is active: https://github.com/qnib/qcollect-ng/issues/14
 			pre := qutils.TransformFsouzaToDocker(stats.PreCPUStats)
 			cur := qutils.TransformFsouzaToDocker(stats.CPUStats)
 			cpuStats := qutils.DiffCPUStats(pre, cur)
 			cs.qChan.Data.Send(qtypes.NewExt("input", "docker-stats", "cpu.system.ms", qtypes.Gauge, float64(cpuStats.SystemUsage/10000000), dim, stats.Read, false))
 			cs.qChan.Data.Send(qtypes.NewExt("input", "docker-stats", "cpu.usage.ms", qtypes.Gauge, float64(cpuStats.CPUUsage.TotalUsage/10000000), dim, stats.Read, false))
+			*/
 			cs.qChan.Data.Send(qtypes.NewExt("input", "docker-stats", "memory.usage.bytes", qtypes.Gauge, float64(stats.MemoryStats.Usage), dim, stats.Read, false))
 			cs.qChan.Data.Send(qtypes.NewExt("input", "docker-stats", "memory.limit.bytes", qtypes.Gauge, float64(stats.MemoryStats.Limit), dim, stats.Read, false))
 			cs.qChan.Data.Send(qtypes.NewExt("input", "docker-stats", "pid.current.count", qtypes.Gauge, float64(stats.PidsStats.Current), dim, stats.Read, false))
 			cs.qChan.Data.Send(qtypes.NewExt("input", "docker-stats", "net.rx.bytes", qtypes.Gauge, float64(stats.Network.RxBytes), dim, stats.Read, false))
 			cs.qChan.Data.Send(qtypes.NewExt("input", "docker-stats", "net.tx.bytes", qtypes.Gauge, float64(stats.Network.TxBytes), dim, stats.Read, false))
-			*/
 		}
 	}
 }
 
-func ListenDispatcher(qChan fTypes.QChan, dockerHost string) {
-	/*cntClient, err := docker.NewClient(dockerHost)
+func ListenDispatcher(qChan fTypes.QChan, ignReg *regexp.Regexp, dockerHost string, wg sync.WaitGroup) {
+	cntClient, err := docker.NewClient(dockerHost)
 	if err != nil {
 		log.Printf("[EE] Could not connect fsouza/go-dockerclient to '%s': %v", dockerHost, err)
 		return
-	}*/
+	}
 	// Filter start/stop event of a container
 	engineCli, err := client.NewClient(dockerHost, "v1.25", nil, nil)
 	if err != nil {
@@ -120,11 +131,8 @@ func ListenDispatcher(qChan fTypes.QChan, dockerHost string) {
 	// Initialize already running containers
 	cnts, err := engineCli.ContainerList(context.Background(), types.ContainerListOptions{})
 	for _, cnt := range cnts {
-		log.Printf("[II] ### Already running Container %s", cnt.ID)
-		//SuperMap[cnt.ID] = NewCntSuper(cntClient, qChan, cnt.ID, qutils.SanatizeContainerName(cnt.Names))
-		//cs := NewCntSuper(cntClient, qChan, cnt.ID, qutils.SanatizeContainerName(cnt.Names))
-		//go SuperMap[cnt.ID].Run()
-		//go cs.Run()
+		SuperMap[cnt.ID] = NewCntSuper(cntClient, qChan, cnt.ID, qutils.SanatizeContainerName(cnt.Names), ignReg)
+		go SuperMap[cnt.ID].Run()
 	}
 
 	msgs, errs := engineCli.Events(context.Background(), types.EventsOptions{})
@@ -132,17 +140,16 @@ func ListenDispatcher(qChan fTypes.QChan, dockerHost string) {
 		select {
 		case dMsg := <-msgs:
 			if dMsg.Type == "container" {
-				log.Printf("[II] ### Container %s", dMsg.ID)
 				switch dMsg.Action {
 				case "start":
-					log.Printf("[II] Container started ID:%s", dMsg.ID)
-					//SuperMap[dMsg.ID] = NewCntSuper(cntClient, qChan, dMsg.ID, dMsg.Actor.Attributes["name"])
-					//go SuperMap[dMsg.ID].Run()
-				case "die":
-					log.Printf("[II] Container died ID:%s", dMsg.ID)
-					//SuperMap[dMsg.ID].Com <- "died"
+					log.Printf("[II] Container [%s]->'%s' started", dMsg.ID, dMsg.Actor.Attributes["name"])
+					SuperMap[dMsg.ID] = NewCntSuper(cntClient, qChan, dMsg.ID, dMsg.Actor.Attributes["name"], ignReg)
+					go SuperMap[dMsg.ID].Run()
+				case "die", "resize", "attach":
+					//log.Printf("[II] Container [%s]->'%s' died", dMsg.ID, dMsg.Actor.Attributes["name"])
+					SuperMap[dMsg.ID].Com <- dMsg.Action
 				default:
-					log.Printf("[DD] Unused Action: %s", dMsg.Action)
+					log.Printf("[DD] Container [%s]->'%s' unused Action: %s", dMsg.ID, dMsg.Actor.Attributes["name"], dMsg.Action)
 					continue
 				}
 			}
@@ -159,7 +166,8 @@ func Run(qChan fTypes.QChan, cfg config.Config) {
 	dockerHost, _ := cfg.StringOr("collector.docker-stats.docker-host", "unix:///var/run/docker.sock")
 	var wg sync.WaitGroup
 	wg.Add(1)
-	go ListenDispatcher(qChan, dockerHost)
+	ignReg, _ := cfg.StringOr("collector.docker-stats.ignore-regex", "^[a-z]+_[a-z]+$")
+	go ListenDispatcher(qChan, regexp.MustCompile(ignReg), dockerHost, wg)
 	wg.Wait()
 
 }
