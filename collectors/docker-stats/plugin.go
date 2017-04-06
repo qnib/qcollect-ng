07d861f07a812146e088185ebb121d56b589412e
Christian Kniep <christian@qnib.org>
2017-04-06 20:39:07 +0200

    disabled CPU metrics to not panic (#14)

--- collectors/docker-stats/plugin.go
+++ collectors/docker-stats/plugin.go
@@ -3,7 +3,7 @@ package main
 import (
 	"C"
 	"log"
-	//"fmt"
+	"fmt"
 	"sync"
 	"regexp"
 
@@ -11,11 +11,11 @@ import (
 	"golang.org/x/net/context"
 	"github.com/docker/docker/api/types"
 	"github.com/fsouza/go-dockerclient"
-	//"github.com/pkg/errors"
+	"github.com/pkg/errors"
 	"github.com/zpatrick/go-config"
 
 	"github.com/qnib/qcollect-ng/utils"
-	//"github.com/qnib/qcollect-ng/types"
+	"github.com/qnib/qcollect-ng/types"
 	fTypes "github.com/qnib/qframe/types"
 )
 
@@ -67,16 +67,15 @@ func (cs ContainerSupervisor) Run() {
 			switch msg {
 			case "died":
 				log.Printf("[DD] Container [%s]->'%s' died -> BYE!", cs.CntID, cs.CntName)
-				break
+				return
 			default:
 				log.Printf("[DD] Container [%s]->'%s' got message from cs.Com: %v\n", cs.CntID, cs.CntName, msg)
 			}
-		/*
 		case stats, ok := <-statsChannel:
 			if !ok {
 				err := errors.New(fmt.Sprintf("Bad response getting stats for container: %s", cs.CntID))
 				log.Println(err.Error())
-				break
+				return
 			}
 
 			dim := map[string]string{
@@ -92,22 +91,25 @@ func (cs ContainerSupervisor) Run() {
 				dim["task_slot"] = task.Slot
 				dim["service_name"] = task.Name
 			}
+
+			/*
+			//////// Panic when this is active: https://github.com/qnib/qcollect-ng/issues/14
 			pre := qutils.TransformFsouzaToDocker(stats.PreCPUStats)
 			cur := qutils.TransformFsouzaToDocker(stats.CPUStats)
 			cpuStats := qutils.DiffCPUStats(pre, cur)
 			cs.qChan.Data.Send(qtypes.NewExt("input", "docker-stats", "cpu.system.ms", qtypes.Gauge, float64(cpuStats.SystemUsage/10000000), dim, stats.Read, false))
 			cs.qChan.Data.Send(qtypes.NewExt("input", "docker-stats", "cpu.usage.ms", qtypes.Gauge, float64(cpuStats.CPUUsage.TotalUsage/10000000), dim, stats.Read, false))
+			*/
 			cs.qChan.Data.Send(qtypes.NewExt("input", "docker-stats", "memory.usage.bytes", qtypes.Gauge, float64(stats.MemoryStats.Usage), dim, stats.Read, false))
 			cs.qChan.Data.Send(qtypes.NewExt("input", "docker-stats", "memory.limit.bytes", qtypes.Gauge, float64(stats.MemoryStats.Limit), dim, stats.Read, false))
 			cs.qChan.Data.Send(qtypes.NewExt("input", "docker-stats", "pid.current.count", qtypes.Gauge, float64(stats.PidsStats.Current), dim, stats.Read, false))
 			cs.qChan.Data.Send(qtypes.NewExt("input", "docker-stats", "net.rx.bytes", qtypes.Gauge, float64(stats.Network.RxBytes), dim, stats.Read, false))
 			cs.qChan.Data.Send(qtypes.NewExt("input", "docker-stats", "net.tx.bytes", qtypes.Gauge, float64(stats.Network.TxBytes), dim, stats.Read, false))
-			*/
 		}
 	}
 }
 
-func ListenDispatcher(qChan fTypes.QChan, ignReg *regexp.Regexp, dockerHost string) {
+func ListenDispatcher(qChan fTypes.QChan, ignReg *regexp.Regexp, dockerHost string, wg sync.WaitGroup) {
 	cntClient, err := docker.NewClient(dockerHost)
 	if err != nil {
 		log.Printf("[EE] Could not connect fsouza/go-dockerclient to '%s': %v", dockerHost, err)
@@ -165,7 +167,7 @@ func Run(qChan fTypes.QChan, cfg config.Config) {
 	var wg sync.WaitGroup
 	wg.Add(1)
 	ignReg, _ := cfg.StringOr("collector.docker-stats.ignore-regex", "^[a-z]+_[a-z]+$")
-	go ListenDispatcher(qChan, regexp.MustCompile(ignReg), dockerHost)
+	go ListenDispatcher(qChan, regexp.MustCompile(ignReg), dockerHost, wg)
 	wg.Wait()
 
 }
